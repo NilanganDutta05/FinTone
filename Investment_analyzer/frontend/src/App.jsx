d8988490f43a35c30d6e5eed156a89f511c8d3e7
NilanganDutta05 <nilangandutta05official@gmail.com>
2026-03-06 22:21:38 +0530

    Analytics inclusion

--- Investment_analyzer/frontend/src/App.jsx
+++ Investment_analyzer/frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Analytics/>
     </>
   );
 }

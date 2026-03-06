c63c6ca3560dda96959e7ddbf2882ca2efd47f47
NilanganDutta05 <nilangandutta05official@gmail.com>
2026-03-06 22:25:41 +0530

    reverting

--- Investment_analyzer/frontend/src/App.jsx
+++ Investment_analyzer/frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
-      <Analytics/>
     </>
   );
 }
